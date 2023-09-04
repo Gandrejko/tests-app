9ddb0b4dffd4d53daa861dd24c621399fb94c84a
Andrew <gandrejko2002@gmail.com>
2023-09-04 19:29:47 +0300

    filled login page

--- client/src/pages/login-page/LoginPage.styles.ts
+++ client/src/pages/login-page/LoginPage.styles.ts
@@ -1,5 +1,29 @@
 import { SxProps } from '@mui/material/styles';
 
 export const loginPage: SxProps = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+};
 
+export const loginForm: SxProps = {
+  width: "100%",
+  maxWidth: "400px",
+  padding: "20px",
+  borderRadius: "10px",
+  textAlign: "center",
+};
+
+export const loginButton: SxProps = {
+  marginTop: "20px",
+};
+
+export const signUp: SxProps = {
+  marginTop: "20px",
+};
+
+export const signUpLink: SxProps = {
+  textDecoration: "none",
+  color: "black",
 };
--- client/src/pages/login-page/LoginPage.tsx
+++ client/src/pages/login-page/LoginPage.tsx
@@ -1,11 +1,53 @@
-import { FC } from 'react';
-import { Box } from '@mui/material';
+import React, { FC } from 'react';
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { AppInput } from "../../components/app-input/AppInput";
 
 import * as styles from './LoginPage.styles';
 
 export const LoginPage: FC = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleRegisterClick = () => {
+    // Логіка для реєстрації користувача
+    // Ви можете викликати API для реєстрації тут
+  };
   return (
     <Box sx={styles.loginPage}>
+      <Box sx={styles.loginForm}>
+        <Typography variant="h4">Sign In</Typography>
+        <AppInput
+          placeholder="Username"
+          fullWidth
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <AppInput
+          placeholder="Password"
+          fullWidth
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={handleRegisterClick}
+          sx={styles.loginButton}
+        >
+          Sign In
+        </Button>
+        <Typography sx={styles.signUp}>
+          Don't have an account?
+          <Link to="/register">
+            <Typography sx={styles.signUpLink}>
+              Sign Up
+            </Typography>
+          </Link>
+        </Typography>
+      </Box>
     </Box>
   );
 };
