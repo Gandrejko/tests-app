fe9b5ff91093947edc9ab666caf14877fd54d47d
Andrew <gandrejko2002@gmail.com>
2023-09-05 12:18:06 +0300

    rewrote interfaces

--- server/src/interfaces/questions.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Question {
-  name: string;
-  testId: string;
-}
--- server/src/interfaces/tests.interface.ts
+++ server/src/interfaces/tests.interface.ts
@@ -1,5 +1,15 @@
+export interface Option {
+  name: string;
+  isCorrect: boolean;
+}
+
+export interface Question {
+  name: string;
+  options: Option[];
+}
+
 export interface Test {
   name: string;
   description: string;
-  creatorId: string;
+  questions: Question[];
 }
