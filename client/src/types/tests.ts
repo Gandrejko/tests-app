6df514bfc40dc0c43053e62e01d4acf863a8f007
Andrew <gandrejko2002@gmail.com>
2023-09-05 16:26:27 +0300

    rewrote types

--- client/src/types/tests.ts
+++ client/src/types/tests.ts
@@ -1,20 +1,15 @@
 export type Option = {
-  id: string;
-  createdAt: string;
   name: string;
   isCorrect: boolean;
 }
 
 export type Question = {
-  id: string;
-  createdAt: string;
   name: string;
   options: Option[];
 }
 
 export type Test = {
-  id: string;
-  createdAt: string;
+  _id: string;
   name: string;
   description: string;
   questions: Question[];
