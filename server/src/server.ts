406dcc8f73c5aeccd83dabc24b11f0eebd1f3db7
Andrew <gandrejko2002@gmail.com>
2023-09-07 17:33:18 +0300

    simplified server starting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,35 @@
-import { App } from '@/app';
+import "reflect-metadata";
+import { DB_URL, PORT } from "@config";
 import { AuthRoute } from '@routes/auth.route';
 import { TestRoute } from "@routes/tests.route";
+import cors from "cors";
+import express from "express";
+import { connect, ConnectOptions } from "mongoose";
 
-const app = new App([new AuthRoute(), new TestRoute()]);
+const app = express();
 
-app.listen();
+app.use(cors({origin: '*', credentials: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const routes = [new AuthRoute(), new TestRoute()];
+
+routes.forEach(route => {
+  app.use('/', route.router);
+});
+
+const startApp = async () => {
+  try {
+    await connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    } as ConnectOptions);
+    app.listen(PORT, () => {
+      console.log(`🚀 App listening on the port ${PORT}`)
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+startApp();
